550a205ff969a98cf5256fd20f5f3434760a21aa
mabidi Tshilidzi <Mabidi2287@gmail.com>
2025-07-05 00:39:16 +0200

    mindscape modules and database configuration

---
+++ mindscape-restController-api-Impl/src/main/java/za/robo/MindscapeBackendApplication.java
@@ -0,0 +1,11 @@
+package za.robo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindscapeBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MindscapeBackendApplication.class, args);
+    }
+}
